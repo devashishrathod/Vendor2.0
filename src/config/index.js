00056251e4755bb4d33ac3888c7cf914eb6ad4e3
Devashish Rathod <rathod.devashish09@gmail.com>
2026-06-18 09:00:25 +0530

    env and variables updated

--- src/config/index.js
+++ src/config/index.js
@@ -1,9 +1,1 @@
-import { CONSTANT_VITE_ENV } from "../constants";
-
-const VITE_ENV =
-  import.meta.env.VITE_ENVIRONMENT || CONSTANT_VITE_ENV.DEVELOPMENT;
-
-export const BASE_URL =
-  VITE_ENV === CONSTANT_VITE_ENV.DEVELOPMENT
-    ? import.meta.env.VITE_LOCAL_BASE_URL
-    : import.meta.env.VITE_PROD_BASE_URL;
+export const BASE_URL = import.meta.env.VITE_BASE_URL;
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,8 +1,3 @@
-export const CONSTANT_VITE_ENV = Object.freeze({
-  DEVELOPMENT: "DEVELOPMENT",
-  PRODUCTION: "PRODUCTION",
-});
-
 export const ROLES = Object.freeze({
   ADMIN: "ADMIN",
   VENDOR: "VENDOR",
